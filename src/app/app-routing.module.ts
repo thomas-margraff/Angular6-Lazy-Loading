d87683f789bb47461b381bede1d08300eb0d7667
thomas-margraff <tmargraff@gmail.com>
2018-09-21 10:26:49 -0700

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: HomeComponent
   },
   {
-    path: "posts",
-    loadChildren: "../app/posts/posts.module#PostsModule"
+    path: 'posts',
+    loadChildren: '../app/posts/posts.module#PostsModule'
   },
   {
-    path: "king",
-    loadChildren: "../app/king/king.module#KingModule"
+    path: 'king',
+    loadChildren: '../app/king/king.module#KingModule'
+  },
+  {
+    path: 'user',
+    loadChildren: '../app/user/user.module#UserModule'
+  },
+  {
+    path: 'customer',
+    loadChildren: '../app/customer/customer.module#CustomerModule'
   }
 ];
 
--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import {  PostsComponent} from './posts.component'
+import { PostsComponent } from './posts.component';
 
-
-const routes: Routes = [
-    { path: "", component: PostsComponent},
-
-];
+const routes: Routes = [{ path: '', component: PostsComponent }];
 
 @NgModule({
-    exports: [RouterModule],
-    imports: [RouterModule.forChild(routes)]
+  exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)]
 })
-export class PostsRoutingModule { }
+export class PostsRoutingModule {}
